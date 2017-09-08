292423990d0b02b8ac59d7f0c281cc1fe273acda
ZHYZhanghaiyan <623897283@qq.com>
2017-09-08 20:30:25 +0800

    提交 by zhy..

---
+++ gitdemo/src/main/java/gitdemo/GitTest.java
@@ -0,0 +1,11 @@
+package gitdemo;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("This is my first git-demo!");
+	}
+
+}
