92c0365e2a7b5236fb2ebac60599cfa6730f2cd1
ZHYZhanghaiyan <623897283@qq.com>
2017-09-08 17:39:56 +0800

    首次提交by zhy.

---
+++ testgithello/src/main/java/testgithello/GitHelloDemo.java
@@ -0,0 +1,11 @@
+package testgithello;
+
+public class GitHelloDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is a git-hello demo!");
+	}
+
+}
